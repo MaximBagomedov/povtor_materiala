a4bba8007070b537dfed669883f2693f21129dc6
unknown <arsenbagomedov@gmail.com>
2023-09-07 16:52:46 +0300

    Zadacha1-3

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿/*Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a * a;
+Console.WriteLine(b);
+*/
+
+//задача 2
+
+/*Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = number2 * number2;
+if (result == number1){
+    Console.WriteLine($"a = {number1}, b = {number2} -> да ");  
+}
+else {
+    Console.WriteLine($"a = {number1}, b = {number2} -> нет");
+    
+}
+
+/* Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда 
+5 -> Пятница*/
+
+
+/*Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case  1:
+    Console.WriteLine($"{number} -> Понидельник");
+       break;
+    case  2:
+    Console.WriteLine($"{number} -> Вторник");
+       break;
+    case  3:
+    Console.WriteLine($"{number} -> Среда");
+       break;
+    case  4:
+    Console.WriteLine($"{number} -> Четверг");
+       break;
+    case  5:
+    Console.WriteLine($"{number} -> Пятница");
+       break;
+    case  6:
+    Console.WriteLine($"{number} -> Суббота");
+       break;
+    case  7:
+    Console.WriteLine($"{number} -> Воскресенье");
+       break;
+    default:
+    Console.WriteLine($"Вы не ввели число или указали не верное число");
+    
+        break;
+}
+
+*/
+
+
